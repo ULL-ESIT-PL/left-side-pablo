84e84e518d36e39f49aa70577db2e9af294dd09c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-02-06 12:57:10 +0000

    working in the tests for the plugin

--- packages/babel-plugin-left-side-plugin/test/test-description.test.js
+++ packages/babel-plugin-left-side-plugin/test/test-description.test.js
@@ -1,15 +1,22 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
-const input = fs.readdirSync("./in", {encoding:"utf-8"});
-const execOut = new Set(fs.readdirSync("./exec_out", {encoding:"utf-8"}));
+const path = require("path");
+const inputFolder = path.join(__dirname, "in");
+const execOutFolder = path.join(__dirname, "exec_out");
+const input = fs.readdirSync(inputFolder, {encoding:"utf-8"});
+const execOut = new Set(fs.readdirSync(execOutFolder, {encoding:"utf-8"}));
+const outFolder = path.join(__dirname, "out");
+let configFile = path.join(__dirname, "babel.config.js");
 
 for (let testFile of input) {
   test(testFile, () => {
+    const fullTestFile = path.join(inputFolder, testFile);
     expect(execOut.has(testFile)).toBeTruthy();
-    const output = execSync(`npx babel ./in/${testFile}`, {encoding: "utf-8"}).trim();
-    const outputPath = `./out/${testFile}`;
+    const output = execSync(`npx babel --config-file ${configFile} ${fullTestFile}`, {encoding: "utf-8"}).trim();
+    const outputPath = path.join(outFolder, testFile);
     fs.writeFileSync(outputPath, output, {encoding: "utf-8"});
     let execResult = execSync(`node --no-warnings ${outputPath}`, {encoding: "utf-8"}).trim();
-    expect(execResult).toBe(fs.readFileSync(`./exec_out/${testFile}`, {encoding: "utf-8"}).trim());
+    const execOutTestFile = path.join(execOutFolder, testFile);
+    expect(execResult).toBe(fs.readFileSync(execOutTestFile, {encoding: "utf-8"}).trim());
   })
 }
