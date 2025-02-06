369790c6b23d14ea2026af4d498e197715e9ce74
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-02-06 15:35:07 +0000

    test works

--- packages/babel-plugin-left-side-plugin/test/test-description.test.js
+++ packages/babel-plugin-left-side-plugin/test/test-description.test.js
@@ -7,6 +7,7 @@ const input = fs.readdirSync(inputFolder, {encoding:"utf-8"});
 const execOut = new Set(fs.readdirSync(execOutFolder, {encoding:"utf-8"}));
 const outFolder = path.join(__dirname, "out");
 let configFile = path.join(__dirname, "babel.config.js");
+const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/ug; // Remove ANSI escape codes
 
 for (let testFile of input) {
   test(testFile, () => {
@@ -15,8 +16,8 @@ for (let testFile of input) {
     const output = execSync(`npx babel --config-file ${configFile} ${fullTestFile}`, {encoding: "utf-8"}).trim();
     const outputPath = path.join(outFolder, testFile);
     fs.writeFileSync(outputPath, output, {encoding: "utf-8"});
-    let execResult = execSync(`node --no-warnings ${outputPath}`, {encoding: "utf-8"}).trim();
-    const execOutTestFile = path.join(execOutFolder, testFile);
+    let execResult = execSync(`node --no-warnings ${outputPath}`, {encoding: "utf-8"}).replace(ansiRegex, "").trim();
+    const execOutTestFile = path.join(execOutFolder, testFile).toString();
     expect(execResult).toBe(fs.readFileSync(execOutTestFile, {encoding: "utf-8"}).trim());
   })
 }
