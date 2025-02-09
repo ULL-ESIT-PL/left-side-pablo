00e3bdf5dce0c24899dc2690c18ef38bf7df74f7
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-02-09 07:59:20 +0000

    renamed babel-plugin-left-side-plugin/test/error/ to compileerror; examples of calling with multiparameters working

--- packages/babel-plugin-left-side-plugin/test/test-withcompileerrors.test.js
+++ packages/babel-plugin-left-side-plugin/test/test-withcompileerrors.test.js
@@ -10,15 +10,12 @@ const inputFolder = path.join(__dirname, "compileerror");
  *  stdout: string,
  *  stderr: string
  * }
- * @param {*} configFile string
- * @param {*} testFile string
+ * @param {string} command
  * @returns Promise<CliResult>
  */
-async function cli(configFile, testFile) {
+async function cli(command) {
   return new Promise(resolve => {
-    let fullConfigFile = path.join(__dirname, configFile);
-    let fullTestFile = path.join(inputFolder, testFile);
-    exec(`npx babel --config-file ${fullConfigFile} ${fullTestFile}`, { encoding: "utf-8" },
+    exec(command, { encoding: "utf-8" },
       (error, stdout, stderr) => {
         resolve({
           code: error && error.code ? error.code : 0,
@@ -30,7 +27,7 @@ async function cli(configFile, testFile) {
   })
 }
 
-async function main() {
+async function testCompileTimeErrors() {
   const input = fs.readdirSync(inputFolder, { encoding: "utf-8" });
   let configFile = "babel.config.js";
 
@@ -38,7 +35,10 @@ async function main() {
     let errorPattern = require(path.join(__dirname, 'errorpattern', testFile));
     test(testFile, async (done) => {
       try {
-        let {code, error, stdout, stderr } = await cli(configFile, testFile)
+        let fullConfigFile = path.join(__dirname, configFile);
+        let fullTestFile = path.join(inputFolder, testFile);
+        let command = `npx babel --config-file ${fullConfigFile} ${fullTestFile}`;
+        let {code, error, stdout, stderr } = await cli(command, testFile)
         expect(errorPattern({code, error, stdout, stderr })).toBe(true);
         done();
       } catch (e) {
@@ -48,4 +48,29 @@ async function main() {
   }
 }
 
-main();
+testCompileTimeErrors();
+
+/*
+
+const runTimeInputFolder = path.join(__dirname, "runtimeerror");
+
+async function testRunTimeErrors() {
+  const input = fs.readdirSync(runTimeInputFolder, { encoding: "utf-8" });
+  let configFile = "babel.config.js";
+
+  for (let testFile of input) {
+    let errorPattern = require(path.join(__dirname, 'errorpattern', testFile));
+    test(testFile, async (done) => {
+      try {
+        let compileResult = await cli(configFile, testFile)
+        expect(compileResult.code).toBe(0)
+
+        expect(errorPattern({code, error, stdout, stderr })).toBe(true);
+        done();
+      } catch (e) {
+        console.log(e.message);
+      }
+    })
+  }
+}
+*/
