69d4e081f66fa7abbbf7d702713130713cb42d16
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-04-18 11:16:03 +0100

    example-four-object-cycle.js does not throw an error and does not produces a deep equality interpretation

---
+++ examples/example-four-object-cycle.js
@@ -0,0 +1,27 @@
+function @@ foo(bar) {
+  return 0;
+}
+// Fails when using hash. Hashes:
+// object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:string:12:[CIRCULAR:0],,,,
+// object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:string:12:[CIRCULAR:1],,,,,
+// In [CIRCULAR:1], the number stands for a counter: the number of evaluated object. This is the way object-hash deals with circular references.
+
+let fourthObj = {}
+let thirdObj = {
+  ref: fourthObj
+}
+let secondObj = {
+  ref: thirdObj
+}
+let firstObj = {
+  ref: secondObj
+};
+let equalToFirstObj = {
+  ref: secondObj
+};
+fourthObj["ref"] = firstObj
+
+
+foo(firstObj) = 1;
+console.log(foo(equalToFirstObj)); // 0 When running with StoreMapWithHash. It is considered a different object. No error is thrown.
+console.log(foo(firstObj));        // 1
