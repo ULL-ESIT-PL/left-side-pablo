46a078745e2bf17978da0dedd2e54cc066ea8ebf
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-04-19 11:25:21 +0100

    moved cycled object tests to in_error (example-four-object-cycle.js for instance)

---
+++ packages/a-test/out/example-four-object-cycle.js
@@ -0,0 +1,26 @@
+const {
+  assign,
+  functionObject
+} = require("@ull-esit-pl/babel-plugin-left-side-support");
+const foo = functionObject(function (bar) {
+  return 0;
+}); // Fails when using hash. Hashes:
+// object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:string:12:[CIRCULAR:0],,,,
+// object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:object:1:string:3:ref:string:12:[CIRCULAR:1],,,,,
+// In [CIRCULAR:1], the number stands for a counter: the number of evaluated object. This is the way object-hash deals with circular references.
+let fourthObj = {};
+let thirdObj = {
+  ref: fourthObj
+};
+let secondObj = {
+  ref: thirdObj
+};
+let firstObj = {
+  ref: secondObj
+};
+let equalToFirstObj = {
+  ref: secondObj
+};
+fourthObj["ref"] = firstObj;
+assign(foo, [firstObj], 1, []);
+console.log(foo(equalToFirstObj));
